756761f973fb45b008af100e65afa49b7aac6873
kagreene <kristengreene2023@U.northwestern.edu>
2025-07-14 10:43:59 -0500

    switch to external dictionary API

--- client/src/api/dictionaryAPI.js
+++ client/src/api/dictionaryAPI.js
@@ -2,7 +2,7 @@
 // This file contains the API call to fetch the definition of a word from the server
 // API call matches the route defined in server/routes/dictionary.js
 const fetchDefinition = async (word) => {
-    const response = await fetch(`/api/definition/${word}`);
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     const data = await response.json();
   
     if (!Array.isArray(data) || data.length === 0) {
